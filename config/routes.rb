9120eac3f223bb8c0fbc5ee6004e6b28cd262cb5
Ben Lang <ben.lang@mail.ru>
2025-10-28 14:19:17 +0100

    zwischenstand_gameboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Gameboard route
+  get "/gameboard", to: "gameboard#show", as: :gameboard
+
+  # Challenges routes
+  resources :challenges, only: [:index, :show, :create]
+
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
