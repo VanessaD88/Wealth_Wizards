cf5d2593f3a835b6e4b6ca506182a61f8706f9f2
Alexander Haardt <haardt.alexander@gmail.com>
2025-10-29 11:29:14 +0100

    Basic Question implementation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,17 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def gameboard
+    @user = current_user
+    @level = @user.level
+    @challenges = @level ? @level.challenges : []
+
+    @challenge =
+      if params[:challenge_id].present?
+        @challenges.find_by(id: params[:challenge_id])
+      else
+        @challenges.order(created_at: :desc).first
+      end
+  end
 end
