3588fb15d9a6ff52f4d4f86b6b0ae5bf16a9b411
Vanessa Deol <vanessa.deol@outlook.de>
2025-10-30 11:59:27 +0100

    Default Level set

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_commit :ensure_default_level, on: :create
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-has_one :level
-has_many :challenges, through: :levels
+  has_one :level
+  has_many :challenges, through: :levels
 
-validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 
   AVATARS = {
     "Penny" => "avatars/penny.png",
@@ -23,4 +24,15 @@ validates :username, presence: true, uniqueness: true
       "avatars/default.png"
     end
   end
+
+  private
+  def ensure_default_level
+    return if level.present?
+    create_level!(
+      name: "Level 0",
+      description: "Game not started",
+      completion_status: false
+    )
+  end
+
 end
