d5741d0391efa0cc23526a1ebc421aba7ed35d41
Vanessa Deol <vanessa.deol@outlook.de>
2025-11-05 13:41:57 +0100

    ..

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -1,15 +1,43 @@
 import { Controller } from "@hotwired/stimulus"
+console.log(" Charts controller connected");
 
-// Connects to data-controller="charts"
+console.log("Creating chart for:", document.getElementById("level1"))
+
 export default class extends Controller {
+  static targets = ["canvas"];
+
+  worldPopulation = {
+    datasets: [{
+      data: [504, 496]
+    }],
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+      'Men',
+      'Women'
+    ]
+  };
+
   connect() {
-    // Register the plugin globally
-    Chart.register(ChartDataLabels)
+    // TODO: console.log something!
+    console.log("Doughnut Chart Controller connected");
 
-    // Gpt Code
-    
+    const labels = ['Men', 'Women'];
+    const data = [504, 496];
 
-    //Gpt Code End
-
+    this.chart = new Chart(this.canvasTarget, {
+      type: "doughnut",
+      data: {
+        labels,
+        datasets: [{
+          label: "World Population (millions)",
+          data,
+          backgroundColor: [
+            'rgb(54, 162, 235)',
+            'rgb(255, 99, 132)'
+          ],
+          hoverOffset: 4
+        }]
+      }
+    });
   }
 }
